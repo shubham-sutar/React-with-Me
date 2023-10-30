090a587e8556afddb2eb505609eda9d12b32ca44
shubham-sutar <shubhamsutar93@gmail.com>
2023-10-30 10:38:40 +0530

    updated

--- Project/02counter/src/App.jsx
+++ Project/02counter/src/App.jsx
@@ -29,12 +29,24 @@ function App() {
     }
   }
 
+  let [Number, setNumber] = useState(0);
+
+      const RandomNum = () => {
+        let gen = Math.floor(Math.random()*100+1);
+        setNumber(gen);
+      }
+
   return (
     <>
       <h1>Coder Shubham</h1>
       <h2>Counter Value: {counter}</h2><br />
       <button onClick={addValue}>Add Value</button><br /><br />
       <button onClick={rmvValue}>Remove Value</button>
+
+      <br /><br />
+      <h1>Generate Random Number</h1>
+      <button onClick={RandomNum}>Generate</button>
+      <h3>Random Number : {Number}</h3>
     </>
   )
 }
